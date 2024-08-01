4ce280ac80b9a8e4b03ee725250c64fc52ce8edf
jumi <ajsel422@gmail.com>
2024-08-01 14:05:35 +0900

    Comment Create 기능 구현:

--- aaaa.md
+++ aaaa.md
@@ -37,4 +37,28 @@ if request.method == 'POST':
 게시물 테이블과 댓글 테이블 의 관계 형성
 1:N 관계 댓글은 1개의 게시물이라도 있어야 작성 가능(여러개)
 하나의데이터 하위에 여러개의 데이터가 속해 있을 수 있다.
- 
+
+
+1. index.html
+
+{% block body %}
+    <h1>{{article.title}}</h1>
+    <p>{{article.content}}</p>
+    <hr>
+
+    <!-- 부모테이블을 기준으로 속해있는 객체 모두 출력 
+     article.comment_set.all-->
+     <!-- article과 관계가 형성되어있어 따로 url만들지 않아도 출력가능 -->
+    {% for comment in article.comment_set.all %}
+        <li>{{comment.content}}</li>
+    {% endfor %}
+
+{% endblock %}
+
+2.forms.py
+class CommentForm(forms.ModelForm):
+    class Meta():
+        model = Comment
+        field = '__all__' 
+        fields s빼먹으면 에러 ... 
+        fields = ('content',) 튜플형태 
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
-from .forms import ArticleForm
+from .forms import ArticleForm, CommentForm
 
 # Create your views here.
 
@@ -16,9 +16,11 @@ def index(request):
 
 def detail(request, id):
     article = Article.objects.get(id=id)
+    form = CommentForm()
 
     context = {
-        'article': article
+        'article': article,
+        'form': form,
     }
     return render(request, 'detail.html', context)
 
@@ -37,3 +39,26 @@ def create(request):
         'form': form,
     }
     return render(request, 'form.html', context)
+
+
+def comment_create(request, article_id):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False) 
+            # commit=False 완전히 데이터베이스에 넣지않고 임시저장
+            # 1. 객체를 저장하는 방법
+            # article = Article.objects.get(id=article_id)
+            # comment.article = article
+            # comment.save()
+            
+            # 2. integer(숫자)를 저장하는 방법
+            comment.article_id = article_id
+            comment.save()
+
+            
+            return redirect('articles:detail', id=article_id)
+
+
+    else:
+        return redirect('articles:index')
