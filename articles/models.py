c0347e5f5527ece2721e4ed62bc4699c6331a041
jumi <ajsel422@gmail.com>
2024-08-01 11:59:09 +0900

    Comment Read 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    # comment_set = 자식테이블에 접근할 수 있게 장고가 생성
     
 class Comment(models.Model):
     content = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE) 
     # Article 테이블과 관계설정 on_delete(부모테이블이 지워졌을 때)=CASCADE(종속)
+    # article_id = 부모테이블에 접근할 수 있는 변수 
